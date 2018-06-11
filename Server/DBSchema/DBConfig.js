10a14663dccbb170951ce2e34d1b31eb09ae5798
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-06-11 23:30:54 +0530

    Add patientDetails schema

--- Server/DBSchema/DBConfig.js
+++ Server/DBSchema/DBConfig.js
@@ -24,3 +24,34 @@ var CustomerSchema = new Schema({
 var IncomeSchema = new Schema({
 
 })
+
+//IT16033856
+
+var patientDetails = new Schema({
+    fullName: {
+        type: String,
+        require: true
+    },
+    contactNumber: {
+        type: Number,
+        require: true
+    },
+    language: {
+        type: String,
+        require: true
+    },
+    dob: {
+        type: Date,
+        require: true
+    },
+    gender: {
+        type: String,
+        require: true
+    },
+    gudianContactNumber: {
+        type: Number,
+        require: true
+    }
+});
+
+Mongoose.model("patientDetails", patientDetails);
