481025abf0128ce94ae7a097711b2f1b76ddb284
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-06-12 10:46:22 +0530

    complete patient router

--- Server/Routers/patientRouter.js
+++ Server/Routers/patientRouter.js
@@ -8,5 +8,39 @@ router.get("/", function(request, response) {
         response.status(data.status).send({data: data.searchedPatients});
     }).catch(function(err) {
         response.status(err.status).send({message: err.message});
-    })
-})
+    });
+});
+
+router.get("/:id", function(request, response) {
+    patientController.findPatient(request.params.id).then(function(data) {
+        response.status(data.status).send({data: data.searchedPatient});
+    }).catch(function(err) {
+        response.status(err.status).send({message: err.message});
+    });
+});
+
+router.post("/", function(request, response) {
+    patientController.addPatient(request.body).then(function(data) {
+        response.status(data.status).send({message: data.message});
+    }).catch(function(err) {
+        response.status(err.status).send({message: err.message});
+    });
+});
+
+router.put("/:id", function(request, response) {
+    patientController.updatePatient({id: request.params.id}, request.body).then(function(data) {
+        response.status(data.status).send({message: data.message})
+    }).catch(function(err) {
+        response.status(err.status).send({message: err.message});
+    });
+});
+
+router.delete("/:id", function(request, resolve) {
+    patientController.deletePatient({id: request.params.id}).then(function(data) {
+        response.status(data.status).send({message: data.message})
+    }).catch(function(err) {
+        response.status(err.status).send({message: err.message});
+    });
+});
+
+module.exports = router;
