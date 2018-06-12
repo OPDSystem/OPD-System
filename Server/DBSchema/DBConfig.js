3f8776cc240982332b5f041891e7640d0ffab0c0
ranjitha logaratnam <ranjitha logaratnam>
2018-06-12 11:33:53 +0530

    Questionnaire Changes implemented

--- Server/DBSchema/DBConfig.js
+++ Server/DBSchema/DBConfig.js
@@ -89,10 +89,54 @@ var clinicschema= new Schema({
     patients: [string]
 })
 
+var questionnaireSchema=new Schema({
+     question : {
+         type : String 
+},
+    answer1 : {
+         type : String
+},
+    answer2 : {
+          type : String
+},
+    answer3 : {
+         type : String
+},
+    answer4 : {
+        type : String
+},
+    answer5 : {
+         type : String
+}
+});
+
+var patientQueue= new Schema({
+    patientId: {
+         type: Schema.Types.ObjectId, ref: 'patient'
+    },
+         doctorId: {
+             type: Schema.Types.ObjectId, ref: 'systemUser'
+    },
+         relative: {
+             type: String, required: false
+    },
+        illness: {
+            type: String, required: true
+        }, // nature of the illness
+         status: {
+             type: String, required: true, default: 1
+        }, // 1 - On the list, 0 - Appointed
+         createdDateTime: {
+              type: Date, default: Date.now 
+        },
+})
+
 Mongoose.model("patientDetails", patientDetails);
 Mongoose.model('Customer',CustomerSchema);
 Mongoose.model('Income', IncomeSchema);
 Mongoose.model('clinic',clinicschema);
+Mongoose.model('Quessionnaire',questionnaireSchema);
+Mongoose.model('Queue',patientQueue);
 
 Mongoose.connect('mongodb://127.0.0.1:27017/OPD', function (err) {
     if(err)
--- Server/Routes.js
+++ Server/Routes.js
@@ -1,6 +1,8 @@
 const Express = require('express');
 const Routes = Express.Router();
 var PaymentRoute = require('./Routers/PaymentRouter');
+var QuestionnaireRoute= require('./Routers/questionnaireRouter');
+var QueueRoute= require('./Routers/queueRouter');
 
 Routes.use('/payment/',PaymentRoute);
 
