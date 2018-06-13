92e85d19035669d4e65712778560aba2a33c6d3f
JegaVithu <jeganathan.vithusan@my.sliit.lk>
2018-06-14 01:23:16 +0530

    testing

--- Client/public/Income.jsx
+++ Client/public/Income.jsx
@@ -10,38 +10,29 @@ export default class income extends Component {
         super(props);
         this.state={
             Income:[],
-            Amount:[],
-            total:Number
+            
         }
-        this.total = 0;
+        
     }
 
     getdayincome(data){
         axios.get('http://localhost:8080/payment/income/'+data).then(res => {
             this.setState({
                 Income: res.data.data,
-                total:res.data.data.amount
+                
             })
         })
-       // this.total = this.state.Income.amount;
+       
     }
 
-    getdayamount(data){
-        axios.get('http://localhost:8080/payment/income/fee'+data).then(res => {
-            this.setState({
-                Amount: res.data.data
-            })
-        })
-    }
-
+   
     
     
     render() {
         return <div>
             <h3>income</h3>
             <Getdate getdayincome={data => this.getdayincome(data)} getdayamount={ data => this.getdayamount(data)}/>
-            <Income Income={this.state.Income} getdayincome = {() => this.getdayincome()} Amount={this.state.Amount} getdayamount={()=> this.getdayamount()}/>
-            <h3>total :{this.total}</h3>
+            <Income Income={this.state.Income} getdayincome = {() => this.getdayincome()} />
         </div>;
     }
 }
