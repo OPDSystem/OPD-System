16f1b9dafa4c06e522e3886d84797c851753c7ea
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-06-14 02:15:47 +0530

    update project

--- Server/Routes.js
+++ Server/Routes.js
@@ -1,16 +1,17 @@
-const Express = require('express');
+const Express = require("express");
 const Routes = Express.Router();
-var PaymentRoute = require('./Routers/PaymentRouter');
-const patientRouter = require("./Routers/patientRouter")
-var QuestionnaireRoute= require('./Routers/questionnaireRouter');
-var QueueRoute= require('./Routers/queueRouter');
-var clinicRoute=require('./Routers/clinicRouter');
+var PaymentRoute = require("./Routers/PaymentRouter");
+const patientRouter = require("./Routers/patientRouter");
+var QuestionnaireRoute = require("./Routers/questionnaireRouter");
+var QueueRoute = require("./Routers/queueRouter");
+var clinicRoute = require("./Routers/clinicRouter");
+var appointmentRoute = require("./Routers/appointmentRouter");
 
-Routes.use('/payment/',PaymentRoute);
-Routes.use('/patients/',patientRouter);
-Routes.use('/Questionnaire/',QuestionnaireRoute);
-Routes.use('/queue/',QueueRoute);
-Routes.use('/clinic/',clinicRoute);
-
+Routes.use("/payment/", PaymentRoute);
+Routes.use("/patients/", patientRouter);
+Routes.use("/Questionnaire/", QuestionnaireRoute);
+Routes.use("/queue/", QueueRoute);
+Routes.use("/clinic/", clinicRoute);
+Routes.use("/appointment/", appointmentRoute);
 
 module.exports = Routes;
