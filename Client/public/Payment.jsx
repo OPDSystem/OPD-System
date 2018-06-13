08331b1a37a2c5ac6cf414d55618c52a27623a4d
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-06-13 17:51:55 +0530

    bug fixed

--- Client/public/Payment.jsx
+++ Client/public/Payment.jsx
@@ -1,53 +1,57 @@
-'use strict';
+"use strict";
 
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import Income from './Income';
-import Invoice from './Invoice';
-import Paymentdetails from './Paymentdetails';
+import Income from "./Income";
+import Invoice from "./Invoice";
+import Paymentdetails from "./Paymentdetails";
 
 export default class Payment extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-        
-    }
-
-   
-
-
-
-    render() {
-        return <Payment>
+  render() {
+    return (
+      <Payment>
         <div>
-            <h3>payment</h3>
-            <Router>
-                <div>
-                    <div className="row">
-                        <div className="col-sm">
-                            <Link to="/Invoice">Invoice</Link>
-                        </div>
-                        <div className="col-sm">
-                            <Link to="/Income">Income</Link>
-                        </div>
-                        <div className="col-sm">
-                            <Link to="/Paymentdetails">Paymentdetails</Link>
-                        </div>
-                    </div>
-                    <Route exact path="/Invoice" render={props => {
-                        return <Invoice/>
-                    }}/>
-                    <Route path="/Income" render={props => {
-                        return <Income/>
-                    }}/>
-                    <Route path="/Paymentdetails" render={props => {
-                        return <Paymentdetails/>
-                    }}/>
+          <h3>payment</h3>
+          <Router>
+            <div>
+              <div className="row">
+                <div className="col-sm">
+                  <Link to="/Invoice">Invoice</Link>
                 </div>
-            </Router>
-            
-        </div>;
-        </Payment>
-    }
-
+                <div className="col-sm">
+                  <Link to="/Income">Income</Link>
+                </div>
+                <div className="col-sm">
+                  <Link to="/Paymentdetails">Paymentdetails</Link>
+                </div>
+              </div>
+              <Route
+                exact
+                path="/Invoice"
+                render={props => {
+                  return <Invoice />;
+                }}
+              />
+              <Route
+                path="/Income"
+                render={props => {
+                  return <Income />;
+                }}
+              />
+              <Route
+                path="/Paymentdetails"
+                render={props => {
+                  return <Paymentdetails />;
+                }}
+              />
+            </div>
+          </Router>
+        </div>
+      </Payment>
+    );
+  }
 }
