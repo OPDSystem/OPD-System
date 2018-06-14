751fadc0052c0f20868738ede908fcab1f4b3d42
JegaVithu <jeganathan.vithusan@my.sliit.lk>
2018-06-15 03:00:58 +0530

    my frontend design update and main design and update queue

--- Client/QueueView/SearchQueue.jsx
+++ Client/QueueView/SearchQueue.jsx
@@ -55,9 +55,16 @@ export default class Myqueue extends Component{
         return <div>
             <form onSubmit={event => this.onSubmit(event)}>
                 <div>
-                <label>DoctorID:</label>
+                <label>Doctor Name:</label>
                 <div />
-                <input type="text" onChange={event => this.onNameChange(event)}/>
+                <select onChange={event => this.onNameChange(event)}>
+                   <option value="none">Select Doctor</option>
+                   <option value="Yaathra">Yaathra</option>
+                   <option value="Diva TJ">Diva TJ</option>
+                   <option value="Keerthana Vije">Keerthana Vije</option>
+                   <option value="Tharish Thiva">Tharish Thiva</option>
+                   <option value="Mithra">Mithra</option>
+                   </select>
                 <div />
                 <button type="submit">Find</button>
                 </div>
