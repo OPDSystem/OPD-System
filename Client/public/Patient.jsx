5026b9840453832c5805870fcfd1da50a7b87caf
Sankeeth Naguleswaran <n.sankeeth@gmail.com>
2018-06-15 09:03:04 +0530

    Update front end

--- Client/public/Patient.jsx
+++ Client/public/Patient.jsx
@@ -1,43 +1,59 @@
-'use strict';
+"use strict";
 
-import React, {Component} from 'react';
-import axios                from 'axios';
-import AddPatient  from '../Patient/AddPatient';
-import Register from '../Patient/Patients';
+import React, { Component } from "react";
+import axios from "axios";
+import AddPatient from "../Patient/AddPatient";
+import Register from "../Patient/Patients";
 
 export default class Patient extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-        patients:[]
+  constructor(props) {
+    super(props);
+    this.state = {
+      patients: []
+    };
+    this.getAllPatient();
+  }
+
+  getAllPatient() {
+    axios.get("http://localhost:8080/patients/").then(res => {
+      this.setState({
+        patients: res.data.data
+      });
+    });
+  }
+
+  addPatient(data) {
+    axios
+      .post("http://localhost:8080/patients/", {
+        id: data.id,
+        fullName: data.name,
+        contactNumber: data.contactnum,
+        language: data.language,
+        age: data.Age,
+        gender: data.gender,
+        gudianContactNumber: data.gadcontactnum
+      })
+      .then(result => {
+        if (result.status == 200) {
+          this.getAllPatient();
+          console.log("ADDED!!");
         }
-        this.getAllPatient();
-    }
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
 
-    getAllPatient() {
-        axios.get('http://localhost:8080/patients/').then(res => {
-            this.setState({
-				patients: res.data.data
-			});
-        })
-    }
-
-   	addPatient(data) {
-   		axios.post('http://localhost:8080/patients/',{id:data.id, fullName:data.name, contactNumber:data.contactnum, language:data.language, age:data.Age, gender:data.gender, gudianContactNumber:data.gadcontactnum}).then(result => {
-   			if(result.status == 200) {
-                   this.getAllPatient();
-                   console.log("ADDED!!");
-			}
-		}).catch(err => {
-			alert(err);
-		})
-   	}
-
-   	render() {
-        return <div>
-           <h2>REGISTER PATIENTS</h2>
-            <AddPatient addPatient={data => this.addPatient(data)}/>
-            <Register patients={this.state.patients} getAllPatient = {() => this.getAllPatient()}/>
-        </div>;
-	}
+  render() {
+    return (
+      <div class="mydesign2">
+        <h2>REGISTER PATIENTS</h2>
+        <AddPatient addPatient={data => this.addPatient(data)} />
+        <Register
+          patients={this.state.patients}
+          getAllPatient={() => this.getAllPatient()}
+        />
+      </div>
+    );
+  }
 }
